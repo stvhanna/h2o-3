14d146eba9bace288de2e9609f57afa91d90eb09
Amy Wang <amy@0xdata.com>
2016-12-09 17:28:04 -0800

    Implemented Byte Buffer Wrapper

--- h2o-genmodel/src/main/cxx/h2o/ByteBufferWrapper.h
+++ h2o-genmodel/src/main/cxx/h2o/ByteBufferWrapper.h
@@ -7,37 +7,58 @@
 namespace h2o {
 
 class ByteBufferWrapper {
-public:
+private:
     const VectorOfBytes &_v;
+    int _position;
 
 public:
     ByteBufferWrapper(const VectorOfBytes &v) :
-        _v(v)
+        _v(v),
+        _position(0)
     {}
 
-    int get1U() {
-        assert(0);
-        // & 0xff;
+    uint8_t get1U() {
+        int result = _v[_position] & 0xff;
+        _position++;
+        return result;
     }
 
     uint16_t get2() {
-        assert(0);
+        return get1U() | (get1U() << 8);
     }
 
     uint32_t get3() {
-        assert(0);
+        return get1U() | (get1U() << 8) | (get1U() << 16);
     }
 
     uint32_t get4() {
-        assert(0);
+        return get1U() | (get1U() << 8) | (get1U() << 16) | (get1U() << 24);
     }
 
     float get4f() {
-        assert(0);
+        uint8_t buf[4];
+#if _LIBCPP_BIG_ENDIAN == 1
+        buf[3] = get1U();
+        buf[2] = get1U();
+        buf[1] = get1U();
+        buf[0] = get1U();
+#elif _LIBCPP_LITTLE_ENDIAN == 1
+        buf[0] = get1U();
+        buf[1] = get1U();
+        buf[2] = get1U();
+        buf[3] = get1U();
+#else
+#error Can not determine endianness.
+#endif
+        float f = *(reinterpret_cast<float*>(&buf[0]));
+        return f;
     }
 
     void skip(int n) {
-        assert(0);
+        if(_position + n >= _v.size()) {
+            throw std::invalid_argument("ByteBufferWrapper skip too big");
+        }
+        _position += n;
     }
 };
 
